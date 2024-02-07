caf5d07933fd76083598c531945b9f5cc1a43bed
jignesh <85551495+jigneshbhai@users.noreply.github.com>
2024-02-07 10:59:36 +0530

    complete sign in with google done

---
+++ frontend/src/components/OAuth.jsx
@@ -0,0 +1,54 @@
+import { Button } from "flowbite-react";
+import React from "react";
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { signInSuccess } from "../redux/user/userSlice";
+
+const OAuth = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handelGoogleClick = async () => {
+    const auth = getAuth(app);
+
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
+    try {
+      const resultFromGoogle = await signInWithPopup(auth, provider);
+    //   console.log(resultFromGoogle);
+      const res = await fetch("http://localhost:5000/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: resultFromGoogle.user.displayName,
+          email: resultFromGoogle.user.email,
+          googlePhotoUrl: resultFromGoogle.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Button
+      type="button"
+      gradientDuoTone="pinkToOrange"
+      onClick={handelGoogleClick}
+    >
+      <AiFillGoogleCircle className="w-6 h-6 mr-2" />
+      Continue with Google
+    </Button>
+  );
+};
+
+export default OAuth;
---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcSmzsjlEDoSfu9M811b_5ohnK5xQwBa8",
+  authDomain: "blogsite-847bb.firebaseapp.com",
+  projectId: "blogsite-847bb",
+  storageBucket: "blogsite-847bb.appspot.com",
+  messagingSenderId: "1020599505997",
+  appId: "1:1020599505997:web:f3a591dd40a356c3c53804",
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
