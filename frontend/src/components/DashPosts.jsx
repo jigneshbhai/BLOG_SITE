f8860feaaf5ec00115a889bdc876c2ad20528b96
jignesh <85551495+jigneshbhai@users.noreply.github.com>
2024-02-12 10:40:32 +0530

    some change to check

--- frontend/src/components/DashPosts.jsx
+++ frontend/src/components/DashPosts.jsx
@@ -16,7 +16,7 @@ const DashPosts = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const res = await fetch(`/api/post/getposts`);
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
